b0a8c4f47f940048d7e30fea723b0bb3faea7a8c
omer1213 <omeryahya1213.com>
2025-03-23 23:20:22 +0500

    after url issue resolved

--- app/page.js
+++ app/page.js
@@ -1,4 +1,21 @@
-import Image from "next/image";
+// import Image from "next/image";
+// import Link from "next/link";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       Omer Yahya
+//       <Link href="/contact">
+//         <button className="ml-20 border-4 bg-amber-400">
+//           contact
+//         </button>
+//       </Link>
+//       <h1 className="mt-20 border-1 border-b">the main page without authentivation...</h1>
+//     </div>
+//   );
+// }
+
+
 import Link from "next/link";
 
 export default function Home() {
@@ -7,9 +24,12 @@ export default function Home() {
       Omer Yahya
       <Link href="/contact">
         <button className="ml-20 border-4 bg-amber-400">
-          contact
+          Contact
         </button>
       </Link>
+
+
+      <h1 className="mt-20 border-1 border-b">The main page without authentication...</h1>
     </div>
   );
 }
---
+++ app/signup/[[...signup]]/page.js
@@ -0,0 +1,13 @@
+
+"use client";
+
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <SignUp />
+    </div>
+  );
+}
+
